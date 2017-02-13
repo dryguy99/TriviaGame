4d5dd6fd7666869f6c67f3aec784a0ac1595fb01
Timothy Dry <timothy.dry@verizon.net>
2017-02-13 03:36:25 -0500

    added more questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,35 +7,35 @@ var qeighty1 = {
 		ans1: "Wind Beneath My Wings",
 		ans2: "Don't Worry, Be Happy",
 		ans3: "Giving You The Best That I Got",
-		ans4: "",
+		ans4: "Fast Car",
 		correctAn: qeighty1.ans1,
 		image: ""
 }
 var qeighty2 = {
-		question: "",
-		ans1: "",
-		ans2: "",
-		ans3: "",
-		ans4: "",
-		correctAn: qeighty1.ans1,
+		question: "In 1989, who win the Grammy for Best Comedy Recording?",
+		ans1: "Whoopi Goldberg - Fontaine: Why Am I Straight?",
+		ans2: "Jonathan Winters - Jonathan Winters Finally Captured",
+		ans3: "George Carlin - What Am I Doing in New Jersey?",
+		ans4: "Robin Williams - Good Morning, Vietnam",
+		correctAn: qeighty2.ans4,
 		image: ""
 }
 var qeighty3 = {
-		question: "",
-		ans1: "",
-		ans2: "",
-		ans3: "",
-		ans4: "",
-		correctAn: qeighty1.ans1,
+		question: "Who won the Grammy for Album of the Year in 1980?",
+		ans1: "Donna Summer - Bad Girls Lyrics",
+		ans2: "Billy Joel - 52nd Street",
+		ans3: "The Doobie Brothers - Minute By Minute",
+		ans4: "Kenny Rogers - The Gambler",
+		correctAn: qeighty3.ans2,
 		image: ""
 }
 var qeighty4 = {
-		question: "",
+		question: "Who won th Grasmmy for Best Rock Vocal Group in 1980",
 		ans1: "",
 		ans2: "",
-		ans3: "",
+		ans3: "The Eagles - Heartache Tonight",
 		ans4: "",
-		correctAn: qeighty1.ans1,
+		correctAn: qeighty4.ans3,
 		image: ""
 }
 var qeighty5 = {
@@ -44,7 +44,7 @@ var qeighty5 = {
 		ans2: "",
 		ans3: "",
 		ans4: "",
-		correctAn: qeighty1.ans1,
+		correctAn: qeighty5.ans1,
 		image: ""
 }
 var qeighty6 = {
@@ -53,7 +53,7 @@ var qeighty6 = {
 		ans2: "",
 		ans3: "",
 		ans4: "",
-		correctAn: qeighty1.ans1,
+		correctAn: qeighty6.ans1,
 		image: ""
 }
 var qeighty7 = {
@@ -62,7 +62,7 @@ var qeighty7 = {
 		ans2: "",
 		ans3: "",
 		ans4: "",
-		correctAn: qeighty1.ans1,
+		correctAn: qeighty7.ans1,
 		image: ""
 }
 var qeighty8 = {
