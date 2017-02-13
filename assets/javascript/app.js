678d2d15f340fa519770f10d3f46962a010be248
Timothy Dry <timothy.dry@verizon.net>
2017-02-13 03:21:15 -0500

    set up page and started addeding questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,104 @@
+/* Trivia Game*/
+
+//Global Variables
+
+var qeighty1 = {
+		question: "Which song won the Grammy for record of the year in 1989?",
+		ans1: "Wind Beneath My Wings",
+		ans2: "Don't Worry, Be Happy",
+		ans3: "Giving You The Best That I Got",
+		ans4: "",
+		correctAn: qeighty1.ans1,
+		image: ""
+}
+var qeighty2 = {
+		question: "",
+		ans1: "",
+		ans2: "",
+		ans3: "",
+		ans4: "",
+		correctAn: qeighty1.ans1,
+		image: ""
+}
+var qeighty3 = {
+		question: "",
+		ans1: "",
+		ans2: "",
+		ans3: "",
+		ans4: "",
+		correctAn: qeighty1.ans1,
+		image: ""
+}
+var qeighty4 = {
+		question: "",
+		ans1: "",
+		ans2: "",
+		ans3: "",
+		ans4: "",
+		correctAn: qeighty1.ans1,
+		image: ""
+}
+var qeighty5 = {
+		question: "",
+		ans1: "",
+		ans2: "",
+		ans3: "",
+		ans4: "",
+		correctAn: qeighty1.ans1,
+		image: ""
+}
+var qeighty6 = {
+		question: "",
+		ans1: "",
+		ans2: "",
+		ans3: "",
+		ans4: "",
+		correctAn: qeighty1.ans1,
+		image: ""
+}
+var qeighty7 = {
+		question: "",
+		ans1: "",
+		ans2: "",
+		ans3: "",
+		ans4: "",
+		correctAn: qeighty1.ans1,
+		image: ""
+}
+var qeighty8 = {
+		question: "",
+		ans1: "",
+		ans2: "",
+		ans3: "",
+		ans4: "",
+		correctAn: qeighty1.ans1,
+		image: ""
+}
+var qeighty9 = {
+		question: "",
+		ans1: "",
+		ans2: "",
+		ans3: "",
+		ans4: "",
+		correctAn: qeighty1.ans1,
+		image: ""
+}
+var qeighty10 = {
+		question: "",
+		ans1: "",
+		ans2: "",
+		ans3: "",
+		ans4: "",
+		correctAn: qeighty1.ans1,
+		image: ""
+}
+var qeighty11 = {
+		question: "",
+		ans1: "",
+		ans2: "",
+		ans3: "",
+		ans4: "",
+		correctAn: qeighty1.ans1,
+		image: ""
+}
+
